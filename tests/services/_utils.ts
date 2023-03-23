eed981963596862789cfd21635e57b0c03ad913d
Juan Jose Cetraro <jjcetraro@gmail.com>
2023-03-23 11:23:02 -0300

    doc: add MPL 2.0 license to tests/services/_utils.ts file

--- tests/services/_utils.ts
+++ tests/services/_utils.ts
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 export function areObjectsEqual(obj1: any, obj2: any): boolean {
   // base cases
   if (typeof obj1 !== typeof obj2) return false;
