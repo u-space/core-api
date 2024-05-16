9a567ec36237abf64fe8d04646986a7d193ebf6b
emi <emialonzo@gmail.com>
2024-05-16 11:54:15 -0300

    run document cron on 00:05 every day

--- src/services/cron.service.ts
+++ src/services/cron.service.ts
@@ -7,6 +7,10 @@
 import { schedule } from "node-cron";
 import { DroneTagServices } from "../apis/drone-tag/dronetag-services";
 import { processOperations } from "./operation-cronjobs.service";
+import {
+  processExpiredDocuments,
+  processNextToExpireDocuments,
+} from "./document-cronjobs.service";
 
 const DRONETATG_INTEGRATION = true;
 
@@ -21,23 +25,30 @@ export class CronService {
 
     console.log("<>    CRON SERVICE     <>");
 
-    const operationCron = schedule("*/30 * * * * *", function () {
+    const documentCron = schedule("5 0 * * *", function () {
+      processExpiredDocuments();
+      processNextToExpireDocuments();
+    });
+
+    const operationCron = schedule("*/10 * * * *", function () {
       processOperations();
       // if (DRONETATG_INTEGRATION) {
       // 	dts.overview();
       // }
     });
+    documentCron.start();
     operationCron.start();
+    this.tasks["documentationCron"] = documentCron;
     this.tasks["operationCron"] = operationCron;
 
-    if (DRONETATG_INTEGRATION) {
-      const dronetagCron = schedule("*/5 * * * * *", function () {
-        // if (DRONETATG_INTEGRATION) {
-        dts.overview();
-        // }
-      });
-      dronetagCron.start();
-      this.tasks["dronetagCron"] = dronetagCron;
-    }
+    // if (DRONETATG_INTEGRATION) {
+    //   const dronetagCron = schedule("*/5 * * * * *", function () {
+    //     // if (DRONETATG_INTEGRATION) {
+    //     dts.overview();
+    //     // }
+    //   });
+    //   dronetagCron.start();
+    //   this.tasks["dronetagCron"] = dronetagCron;
+    // }
   }
 }
