05746c576ce50eff4359992377d5c14dbaecf016
emi <emialonzo@gmail.com>
2024-08-19 14:13:58 -0300

    add fra-service

---
+++ src/apis/fra-server/fra-service.ts
@@ -0,0 +1,47 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import axios from "axios";
+
+import { Operation } from "../../entities/operation";
+import { FRA_URL } from "../../utils/config.utils";
+
+const postCheckOperationPath = "/flightRequest/operationCheck";
+
+export interface IFlightRequestOperationChecks {
+  needAltitude: boolean;
+  needNight: boolean;
+  needVlos: boolean;
+  needGeozones: any[];
+  hasAlitude: boolean;
+  hasNight: boolean;
+  hasVlos: boolean;
+  hasCoordination: any[];
+  validflightRequests: any[];
+}
+
+export class FraService {
+  private axiosInstance = axios.create({
+    baseURL: FRA_URL,
+    timeout: 500000,
+  });
+
+  public async checkOperationConditions(
+    operation: Operation,
+    token: string
+  ): Promise<IFlightRequestOperationChecks> {
+    const responsePositions = await this.axiosInstance.post(
+      postCheckOperationPath,
+      operation,
+      {
+        headers: {
+          auth: token,
+        },
+      }
+    );
+    return responsePositions.data;
+  }
+}
